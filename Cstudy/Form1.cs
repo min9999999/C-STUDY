ac799ee7c7a420c7dd65606e6751ee799b80b0de
min9999999 <117530835+min9999999@users.noreply.github.com>
2025-05-10 15:50:10 +0900

    first commit
    1day winform multithread study

---
+++ Cstudy/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace multithread_1day
+{
+    public partial class Form1 : Form
+    {
+        public Thread thread1 = null;
+        public Thread thread2 = null;
+        public Thread thread3 = null;
+        public Thread thread4 = null;
+        public Thread thread5 = null;
+        public Thread thread6 = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //thread1 = new Thread(new ThreadStart(WorkThread));
+            //thread1.IsBackground = true; //UIThread가 종료될때 해당 쓰레드도 종료.
+            //thread1.Priority = ThreadPriority.Normal; //OS(운영체제)자원을 얼마나 자주 사용할 것인지 할당.
+            //thread1.Start();
+
+            //thread2 = new Thread(new ThreadStart(Work2));
+            //thread2.IsBackground = true; //UIThread가 종료될때 해당 쓰레드도 종료.
+            //thread2.Priority = ThreadPriority.Normal; //OS(운영체제)자원을 얼마나 자주 사용할 것인지 할당.
+            //thread2.Start();
+
+            Form2 form2 = new Form2();
+            form2.Show(); //form2에서 어떠한 작업을 개별적으로 실행.
+
+            Thread.Sleep(5000); //UIThread가 멈추면...
+            //form2가 개별쓰레드로 동작.
+        }
+
+        /// <summary>
+        /// thread가 사용할 함수
+        /// </summary>
+        private void WorkThread()
+        {
+            try
+            {
+                //여기가 실행.
+                Thread.Sleep(2000); //2초동안 해당 쓰레드가 일시중지;
+                                    //MessageBox.Show("BBB테스트"); //별개로 동작 2초후에 //메세지
+                                    //병렬작업
+                                    //오래걸리는 작업.
+                                    //네트워크 전송, 수신, DB작업.
+            }
+            catch (Exception ex)
+            {
+                //오류내용을 Txt..기록.
+            }
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private void Work2()
+        {
+            try
+            {
+                //반복적인 작업.
+                while (true)
+                {
+                    try
+                    {
+
+                    }
+                    catch (Exception ex)
+                    {
+                        throw;
+                    }
+                }
+                //로딩창, 로딩바를 호출해서
+                //여기가 실행.
+                Thread.Sleep(5000); //5초동안 해당 쓰레드가 일시중지;
+                MessageBox.Show("AAA테스트"); //별개로 동작 2초후에
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        //실행될때의 시작속도를 확인해보자.
+
+    }
+}
---
+++ Cstudy/Form2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace multithread_1day
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            //thread1 = new Thread(new ThreadStart(WorkThread));
+            //thread1.IsBackground = true; //UIThread가 종료될때 해당 쓰레드도 종료.
+            //thread1.Priority = ThreadPriority.Normal; //OS(운영체제)자원을 얼마나 자주 사용할 것인지 할당.
+            //thread1.Start();
+
+            //thread2 = new Thread(new ThreadStart(Work2));
+            //thread2.IsBackground = true; //UIThread가 종료될때 해당 쓰레드도 종료.
+            //thread2.Priority = ThreadPriority.Normal; //OS(운영체제)자원을 얼마나 자주 사용할 것인지 할당.
+            //thread2.Start();
+        }
+    }
+}
